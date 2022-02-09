a9d7d53961d798aa3ac6eaa9119caf0b2466c4ee
tanulo <tanulo@barossgabor.local>
2022-02-09 11:36:26 +0100

    teszt

--- feladat/App.js
+++ feladat/App.js
@@ -2,19 +2,21 @@ import * as React from 'react';
 import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import Anyagok from "./Anyagok";
+import Tankok from "./Tankok";
 import Kalkulator from "./Kalkulator";
 import Itempicker from "./Teszt";
+import Harcosok from "./Fighters";
+import Osszeshos from "./Allhero";
 
-function CalcScreen({ navigation }) {
+function BuildCalcScreen({ navigation }) {
   return (
     <Kalkulator/>
   );
 }
 
-function MattersScreen({ navigation }) {
+function TankScreen({ navigation }) {
   return (
-    <Anyagok/>
+    <Tankok/>
   );
 }
 
@@ -24,15 +26,29 @@ function ItemsScreen({ navigation }) {
   );
 }
 
+function FightersScreen({ navigation }) {
+  return (
+    <Harcosok/>
+  );
+}
+
+function AllHeroScreen({ navigation }) {
+  return (
+    <Osszeshos/>
+  );
+}
+
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Tankok">
-        <Drawer.Screen name="Felszerelés kalkulátor" component={CalcScreen} />
-        <Drawer.Screen name="Tankok" component={MattersScreen} />
+      <Drawer.Navigator initialRouteName="Összes hős">
+        <Drawer.Screen name="Összes hős" component={AllHeroScreen} />
+        <Drawer.Screen name="Tankok" component={TankScreen} />
+        <Drawer.Screen name="Harcosok" component={FightersScreen} />
         <Drawer.Screen name="Felszerelés" component={ItemsScreen} />
+        <Drawer.Screen name="Felszerelés kalkulátor" component={BuildCalcScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
