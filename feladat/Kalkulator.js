21d86a22803206b68f07921bb354019d6ed9b1eb
tanulo <tanulo@barossgabor.local>
2022-02-09 11:56:14 +0100

    teszt

--- feladat/Kalkulator.js
+++ feladat/Kalkulator.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { Text, TextInput, View, TouchableOpacity,StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import DropDownPicker from 'react-native-dropdown-picker'
 
 export default class PizzaTranslator extends Component {
   constructor(props) {
@@ -77,7 +79,38 @@ export default class PizzaTranslator extends Component {
        <Text style={{fontSize:10, marginTop: 40,lineHeight: 20}}>3</Text>
       </View>
 
+      <View style={styles.container}>
+      <Text style={styles.paragraph}>
+      React native dropdown picker
+       </Text>
+      <DropDownPicker
+      items={[
+        {label: 'English', value: 'en'},
+        {label: 'Deutsch', value: 'de'},
+        {label: 'French', value: 'fr'},
+    ]}
+    defaultIndex={0}
+    containerStyle={{height: 40}}
+    onChangeItem={item => console.log(item.label, item.value)}
+  />
+  </View>
+
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
